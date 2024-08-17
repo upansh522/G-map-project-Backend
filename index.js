4ff20db8a8a515c7ba6638bb7513fb06c561d352
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-08-18 00:28:47 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = require('./routes/GooglePapPlacesRoute');
 const cors = require('cors');
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = 3000;
+
+const PORT = process.env.BACKEND_URL || 3000;
 const app = express();
 
 app.use(cors());
